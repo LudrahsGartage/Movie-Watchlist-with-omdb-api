d3d10b95fb012d7b15501eb79609991f1b1a232a
LudrahsGartage <101329074+LudrahsGartage@users.noreply.github.com>
2022-11-23 00:11:43 +0530

    fetch request progress

--- index.js
+++ index.js
@@ -1,25 +1,40 @@
 import {apiUrl} from "./apiKey.js"
 
-fetch(`${apiUrl}&s=pokemon`)
-    .then(res => res.json())
-    .then(data => console.log(data))
-
 const inputText = document.getElementById("search-input")
 const searchBtn = document.getElementById("search-btn")
 const moviesContainer = document.querySelector("main")
-let moviesArray = []
+let searchArray = []
+let moviesArray
 
-function setMoviesArray () {
+searchBtn.addEventListener("click", (e) => setMoviesArray(e))
+
+function setMoviesArray (e) {
+    e.preventDefault()
     fetch(`${apiUrl}&s=${inputText.value}`)
-    .then(res => res.json())
-    .then(data => console.log(data))
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            searchArray = data.Search
+            moviesArray = []
+            searchArray.forEach (search => getMovieData(search.Title))
+        })
+}
+
+function getMovieData (title) {
+    let movie
+    fetch(`${apiUrl}&t=${inputText.value}`)
+        .then(res => res.json())
+        .then(data => {
+            movie = data
+        })
+    return movie
 }
 
 function getMovieCardsHTML () {
     return moviesArray.map(movie =>
         (`
         <div class="movie-card">
-            <h2 class="movie-title">Blade Runner</h2>
+            <h2 class="movie-title">${movie.Title}</h2>
             <div class="rating">
                 <i class="fa-solid fa-star"></i>
                 <p class="rating-number">8.0</p>
@@ -27,7 +42,7 @@ function getMovieCardsHTML () {
             <p class="description">A blade runner must pursue and terminate four 
                 replicants who stole a ship in space, and have returned to Earth 
                 to find their creator.</p>
-                <img class="movie-poster" src="https://upload.wikimedia.org/wikipedia/en/9/9b/Blade_Runner_2049_poster.png">
+                <img class="movie-poster" src=${movie.Poster}>
             <div class="details">
                 <p class="run-time">117 min</p>
                 <p class="genre">Action, Drama, Sci-fi</p>
@@ -42,7 +57,7 @@ function getMovieCardsHTML () {
 }
 
 function render() {
-    if (moviesArray.length) {
+    if (searchArray.length) {
         moviesContainer.innerHTML = getMovieCardsHTML
     } else {
         moviesContainer.innerHTML = `
